bbc2194fc657e7e4c82a9c0d5c465a291f1928b1
ARITRA BASU <aritrabasu104@gmail.com>
2021-01-21 17:28:13 +0530

    db config moved to azure key vault

--- src/main/java/com/hcl/poc/config/AppConfig.java
+++ src/main/java/com/hcl/poc/config/AppConfig.java
@@ -1,23 +1,46 @@
 package com.hcl.poc.config;
 
+import javax.sql.DataSource;
+
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Lazy;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-
 @Configuration
 public class AppConfig {
 
+	@Value("${ura-db-url-dev}")
+	private String dbUrl;
+
+	@Value("${ura-db-username-dev}")
+	private String dbUsername;
+
+	@Value("${ura-db-password-dev}")
+	private String dbPassword;
+
+	@Value("${ura-db-driverClassName-dev}")
+	private String dbDriverClass;
 
 	@Lazy
 	@Autowired
 	ObjectMapper mapper;
-	
-	
+
+	@Bean
+	public DataSource getDataSource() {
+		DataSourceBuilder<?> dataSourceBuilder = DataSourceBuilder.create();
+		dataSourceBuilder.driverClassName(dbDriverClass);
+		dataSourceBuilder.url(dbUrl);
+		dataSourceBuilder.username(dbUsername);
+		dataSourceBuilder.password(dbPassword);
+		return dataSourceBuilder.build();
+	}
+
 	@Bean
 	public ModelMapper modelMapper() {
 		ModelMapper modelMapper = new ModelMapper();
